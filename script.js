52c1e54663f585457dffc2ee1b3f8ec89fd66d76
Emmanuel Olakotan <olakotan@gmail.com>
2022-09-24 13:17:28 +0100

    Created a random generator which picks out an element in an array randomly

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Random message generatior project
 
+// I Have decided to use Arrays for the data structure of the messages
+
 
 // all the diffrent star signs 
 const starSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagitarius', 'Capricorn', 'Aquarius', 'Pisces'];
@@ -11,3 +13,23 @@ const posMessage = ['Today is your lucky day', 'You are amazing', 'Your are shin
 const critMessage = ['watch your back', 'work harder','read more', 'learn to code', 'exersice more'];
 
 console.log('Hi i made it finally');
+
+// Create a function 
+const ranGen = (arr) => {
+
+      let ranNum = Math.floor(Math.random()*arr.length);
+      console.log(arr[ranNum]);
+
+}
+
+ranGen(starSign);
+
+// const ranPos = () => {
+    
+// }
+
+// const ranCrit = () => {
+    
+// }
+
+    
