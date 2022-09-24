c5e358c4b0e6a71c835805ed367011d45cb07a68
Emmanuel Olakotan <olakotan@gmail.com>
2022-09-24 10:43:16 +0100

    Random message generatior project

---
+++ script.js
@@ -0,0 +1,13 @@
+// Random message generatior project
+
+
+// all the diffrent star signs 
+const starSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagitarius', 'Capricorn', 'Aquarius', 'Pisces'];
+
+//positive messages 
+const posMessage = ['Today is your lucky day', 'You are amazing', 'Your are shining', 'Good news awaits you'];
+
+// critsism messages
+const critMessage = ['watch your back', 'work harder','read more', 'learn to code', 'exersice more'];
+
+console.log('Hi i made it finally');
